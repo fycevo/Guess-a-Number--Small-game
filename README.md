9151b7a0015fd7ade27bcb763f77a62f7cbdf408
Stofko <114482254+fycevo@users.noreply.github.com>
2022-09-29 21:54:46 +0300

    Source Code
    Added the source code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Guess-a-Number--Small-game
 This is a little game written on JS
+It is not anything special,
+really :D
---
+++ guessTheNumber.js
@@ -0,0 +1,35 @@
+function game(){
+const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    
+    let computerGuess = Math.floor(Math.random()*100);
+    let guess;
+    let recursiveAsyncReadline = function () {
+        readline.question('Guess the number (0-100): ' , number =>
+        {
+            guess = Number(number);
+            if(guess <=100 && guess>= 0){
+                if(guess==computerGuess){
+                    console.log(`You guessed it!`);
+                    return readline.close();
+                }
+                else if(guess<computerGuess){
+                    console.log(`Too low!`);
+                    recursiveAsyncReadline();
+                }
+                else if(guess>computerGuess){
+                    console.log(`Too high!`);
+                    recursiveAsyncReadline();
+                }
+            }
+            else {
+                console.log(`Invalid input! Try again...`);
+                recursiveAsyncReadline();
+            }
+        });
+    }
+    recursiveAsyncReadline();
+}
+game();
